d4f9a48e279aeeef36a8cb8fd269d4e3adf8154d
Samuel Santiago Blanco Galeano <samuelblga@unisabana.edu.co>
2026-04-07 10:10:32 -0500

    Update productos.js

--- JS/productos.js
+++ JS/productos.js
@@ -1,7 +1,3 @@
-// =============================================
-// PRODUCTOS.JS
-// CRUD de productos integrado con Google Sheets.
-// =============================================
 
 // ---- TABLA ----
 
